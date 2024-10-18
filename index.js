88246ef6930418371b5e40883f434f141bfa0fbf
Alumno <gastondavidcejas@gmail.com>
2024-10-18 16:14:34 -0300

    Mini Parcial

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+const tarea = [
+  {
+    "id": 1,
+    "titulo": "Preparar presentación",
+    "descripcion": "Crear la presentación para la reunión del l",
+    "fechaVencimiento": "2024-10-20T18:30:00Z",
+    "estado": {
+      "pendiente": true,
+      "enCurso": false,
+      "finalizada": false
+    }
+  }
+];
+
+app.get("/Tareas", (req, res) => {
+  res.json(tarea);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
